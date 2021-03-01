57d82fd8f97bd4c318a8ba66288d1e0ff9281971
Kate Mills <kate.mills1124@gmail.com>
2021-03-01 14:16:03 -0800

    installed react-is

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,8 +100,8 @@ module.exports = {
         ]
       }
     },
-    `gatsby-plugin-sitemap`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
